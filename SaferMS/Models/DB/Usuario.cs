4217dd7c1d2fb15e3354657514365e8a2799d674
Doroteo Corona <92071824+DoroteoCorona@users.noreply.github.com>
2021-11-11 23:39:16 -0600

    tabla usuarios completada

--- SaferMS/Models/DB/Usuario.cs
+++ SaferMS/Models/DB/Usuario.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 #nullable disable
 
 namespace SaferMS.Models.DB
@@ -8,15 +8,35 @@ namespace SaferMS.Models.DB
     public partial class Usuario
     {
         public int IdUsuario { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public string Apellidos { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public int NumEmpleado { get; set; }
+        
+        [Required(ErrorMessage = "Campo obligatorio"), EmailAddress(ErrorMessage = "Coloca una cuenta de correo valida")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public int Idsexo { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public int IdPuesto { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public string Posición { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public int IdDepartamento { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public string Privilegio { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public string Contraseña { get; set; }
 
         public virtual Departamento IdDepartamentoNavigation { get; set; }
